31fd00a0452d0840ec286da5a55c10c4bc0fc6fe
raihelrdwinov <124329247+raihelrdwinov@users.noreply.github.com>
2023-02-09 14:54:38 +0700

    inisialisasi project

---
+++ views/template/sidebar.php
@@ -0,0 +1,50 @@
+<body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <div class="col-md-3 left_col">
+          <div class="left_col scroll-view">
+            <div class="navbar nav_title" style="border: 0;">
+              <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentelella Alela!</span></a>
+            </div>
+
+            <div class="clearfix"></div>
+
+            <!-- menu profile quick info -->
+            <div class="profile clearfix">
+              <div class="profile_pic">
+                <img src="../../assets/images/img.jpg" alt="..." class="img-circle profile_img">
+              </div>
+              <div class="profile_info">
+                <span>Welcome,</span>
+                <h2>John Doe</h2>
+              </div>
+            </div>
+            <!-- /menu profile quick info -->
+
+            <br />
+
+            <!-- sidebar menu -->
+            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+              <div class="menu_section">
+                <h3>General</h3>
+                <ul class="nav side-menu">
+                  <li><a><i class="fa fa-home"></i> Home <span class="label label-success pull-right"></span></a>
+                  </li>
+                  <li><a><i class="fa fa-user"></i> Pelanggan <span class="label label-success pull-right"></span></a>
+                  </li>
+                  <li><a><i class="fa fa-home"></i> Outlet <span class="label label-success pull-right"></span></a>
+                  </li>
+                  <li><a><i class="fa fa-money"></i> Transaksi <span class="label label-success pull-right"></span></a>
+                  </li>
+                  <li><a><i class="fa fa-group"></i> Registrasi Pelanggan <span class="label label-success pull-right"></span></a>
+                  </li>
+                  <li><a><i class="fa fa-product"></i> Produk <span class="label label-success pull-right"></span></a>
+                  </li>
+              
+              </div>
+
+            </div>
+            <!-- /sidebar menu -->
+
+          </div>
+        </div>
